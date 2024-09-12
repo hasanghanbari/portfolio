f48041987e35e73cb4f8d18f1c18a1197f4244ab
hasan ghanbari <hasanghanbari20@gmail.com>
2024-09-12 11:58:35 +0330

    complete home section1

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Button from "@/components/Button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -14,13 +15,15 @@ export default function Home() {
               <div className="text-orange-500 font-semibold text-[32px]">
                 Hasan Ghanbari
               </div>
-              <div className="text-black-500 font-bold text-[100px]">
-                FrontEnd
+              <div className="relative h-[200px]">
+                <div className="text-black-500 font-bold text-[100px] absolute top-[-30px]">
+                  FrontEnd
+                </div>
+                <div className="text-black-500 font-bold text-[100px] absolute top-[60px] left-[230px]">
+                  Developer
+                </div>
               </div>
-              <div className="text-black-500 font-bold text-[100px]">
-                Developer
-              </div>
-              <div className="text-black-500 text-[21px]">
+              <div className="text-black-500 text-[21px] pb-4">
                 Lorem ipsum dolor sit amet consectetur. Tristique amet sed massa
                 nibh lectus netus in. Aliquet donec morbi convallis pretium.
                 Turpis tempus pharetra
@@ -29,8 +32,55 @@ export default function Home() {
                 <Button>Hire Me</Button>
               </div>
             </div>
+            <div>
+              <div className="mb-[32px]">
+                <Image
+                  src={"/assets/images/image1.png"}
+                  alt={""}
+                  width={538}
+                  height={617}
+                />
+              </div>
+              <div className="flex gap-[25px] justify-center">
+                <Link href={"https://facebook.com"}>
+                  <Image
+                    src={"/assets/images/facebook.png"}
+                    alt={""}
+                    width={32}
+                    height={32}
+                  />
+                </Link>
+                <Link href={"https://x.com"}>
+                  <Image
+                    src={"/assets/images/twitter.png"}
+                    alt={""}
+                    width={32}
+                    height={32}
+                  />
+                </Link>
+                <Link href={"https://instagram.com"}>
+                  <Image
+                    src={"/assets/images/instagram.png"}
+                    alt={""}
+                    width={32}
+                    height={32}
+                  />
+                </Link>
+                <Link href={"https://linkedin.com"}>
+                  <Image
+                    src={"/assets/images/linkedin.png"}
+                    alt={""}
+                    width={32}
+                    height={32}
+                  />
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
+      </section>
+      <section id="section2" className="relative top-[120px]">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8"></div>
       </section>
     </>
   );
